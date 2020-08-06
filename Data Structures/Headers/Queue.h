39ca729e611d3fa36467329eb51d6ea989387e78
WilfredAlmeida <almeidawilfred642@gmail.com>
2020-08-06 23:10:10 +0530

    Added Stack and Queue header files

---
+++ Data Structures/Headers/Queue.h
@@ -0,0 +1,50 @@
+#ifndef Queue
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void queue_enqueue(struct Node *queue, int val)
+{
+    struct Node *t = (struct Node*)malloc(sizeof(struct Node));
+    t->data = val;
+    t->next = NULL;
+
+    if(queue == NULL)
+    {
+        queue = t;
+    }
+    else
+    {
+        struct Node *p = queue;
+
+        while(p->next != NULL)
+            p = p->next;
+        
+        p->next = t;
+    }
+}
+
+struct Node* queue_dequeue(struct Node *queue)
+{
+    struct Node *p = queue;
+    queue = queue->next;
+    return p;
+}
+
+bool is_empty_queue(struct Node *queue)
+{
+    if(queue == NULL)
+        return true;
+
+    return false;
+}
+
+#define Queue
+#endif
