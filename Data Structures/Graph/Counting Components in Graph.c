6e04efdd86a927c5693d9e7424de08af4b8db802
WilfredAlmeida <almeidawilfred642@gmail.com>
2020-08-17 21:38:11 +0530

    Coded: Counting Components in Graph

---
+++ Data Structures/Graph/Counting Components in Graph.c
@@ -0,0 +1,68 @@
+/*
+
+Refer: Code N Code videos
+
+Components are groups of vertices in a graph. If there are 4 vertices say 1,2,3,4
+and 1,2 and 3,4 are connected but no connection exists btwn 1<->3,1<->4,2<->3,2<->4
+than we say there are 2 components in the graph.
+
+In the count function below, we dfs a vertex, so that vertex and its connected
+vertices get added into visited vertices list and so we increment count by one.
+Now we dfs all vertices similarly and if any vertex is not pre-dfs'd or not traversed
+previously would mean that that vertex belongs to different component. So we dfs it
+and increment count.
+
+*/
+
+#include<stdio.h>
+#include"Undirected_List_Graph.h"
+#include"stack.h"
+#define SIZE 4
+
+
+void dfs(struct Node *vertex,struct Node **visited);
+
+int main()
+{
+    struct Node *graph[SIZE] = {NULL};
+
+    undirected_list_graph(graph,SIZE);
+
+    printf("\nNumber of connected components is %d\n",count_connected_components(graph));
+    
+    
+    return 0;
+}
+
+int count_connected_components(struct Node *graph[SIZE])
+{
+    struct Node *visited = NULL;//stackof visited vertices
+
+    int count = 0;
+
+    for(int i=0;i<SIZE;i++)//check all vertices
+    {
+        if(visited == NULL || !stack_search(visited,graph[i]->data))
+        {
+            dfs(graph[i],&visited);
+            count++;
+        }
+    }
+    return count;
+}
+
+void dfs(struct Node *vertex,struct Node **visited)
+{
+    if(!stack_search(*visited,vertex->data))
+    {
+        stack_push(&(*visited),vertex->data);
+
+        struct Node *p = vertex;
+        while(p != NULL)
+        {
+            dfs(p,visited);
+            
+            p=p->next;
+        }
+    }
+}
--- Data Structures/Hello World.c
+++ Data Structures/Hello World.c
@@ -1,77 +1,13 @@
-#include <stdlib.h>
+
 #include <stdio.h>
-struct Node
-{
-    int data;
-    struct Node *next;
-};
-void SortedInsert(struct Node **H, int x)
-{
-    struct Node *t, *q = NULL, *p = *H;
 
-    t = (struct Node *)malloc(sizeof(struct Node));
-    t->data = x;
-    t->next = NULL;
-
-    if (*H == NULL)
-        *H = t;
-    else
-    {
-        while (p && p->data < x)
-        {
-            q = p;
-            p = p->next;
-        }
-        if (p == *H)
-        {
-            t->next = *H;
-            *H = t;
-        }
-        else
-        {
-            t->next = q->next;
-            q->next = t;
-        }
-    }
-}
-struct Node *Search(struct Node *p, int key)
-{
-    while (p != NULL)
-    {
-        if (key == p->data)
-        {
-            return p;
-        }
-        p = p->next;
-    }
-    return NULL;
-}
-
-int hash(int key)
-{
-    return key % 10;
-}
-void Insert(struct Node *H[], int key)
-{
-    int index = hash(key);
-    SortedInsert(&H[index], key);
-}
 int main()
 {
-    struct Node *HT[10];
-    struct Node *temp;
-    int i;
-
-    for (i = 0; i < 10; i++)
-        HT[i] = NULL;
-
-    Insert(HT, 15);
-    Insert(HT, 25);
-    Insert(HT, 35);
-
-    temp = Search(HT[hash(25)], 25);
-
-    printf("%d ", temp->data);
-
+    for(int i=5;i>0;i--)
+    {
+        for(int j=0;j<i;j++)
+            printf("*");
+        printf("\n");
+    }
     return 0;
 }
