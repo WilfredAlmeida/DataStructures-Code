65c238595d793207e903481c4dbdd016ca9947f6
WilfredAlmeida <almeidawilfred642@gmail.com>
2020-08-20 16:43:40 +0530

    Commented: Quick Sort

---
+++ Data Structures/Sorting/Heap Sort.c
@@ -0,0 +1,88 @@
+/*
+
+Refer: Abdul Bari DS
+
+Complete Binary Tree: Binary tree when represented in an array should not contain
+                      any blank spaces. Nodes are represented in the array level-by-
+                      level, and if for any level there is no node than a blank space
+                      is left in the array. For eg: A binary tree at level 2 will
+                      have at max 4 nodes but has 3.
+
+Heap: A Heap is a complete binary tree and:
+      1. Every node should be greater than or equal than its descendents. -> Max Heap
+      2. Every node should be less than or equal than its descendents. -> Min Heap
+
+Operations Performed:
+1. Creation
+2. Insertion
+3. Deletion -> Only root can be deleted
+5. Traversal
+
+*/
+
+#include <stdio.h>
+#define SIZE 10
+
+void insert_into_heap(int *H,int n);
+int delete_from_heap(H, i);
+
+int main()
+{
+
+    int H[] = {0, 10, 20, 30, 25, 5, 40, 35};
+    // 40,25,35,10,5,20,30 -> Formed Heap
+    //Heap is created from index 1, index 0 is blank, ignore it.
+
+    for (int i = 2; i <= 7; i++)
+        insert_into_heap(H, i);
+
+    printf("\nDeleted Value is: %d",delete_from_heap(H,6));
+
+    printf("\n");
+
+    for (int i = 1; i <= 7; i++)
+        printf("%d ", H[i]);
+
+    printf("\n");
+
+    return 0;
+}
+
+void insert_into_heap(int *H,int n)
+{
+    int temp = H[n], i = n;
+
+    while(i > 1 && temp > H[i/2])
+    {
+        H[i] = H[i/2];
+        i = i/2;
+    }
+
+    H[i] = temp;
+}
+
+int delete_from_heap(int *H,int n)
+{
+    int i = 1, j = i*2;
+    int val = H[1];
+    H[1] = H[n];
+    H[n] = val;
+    while(j < n-1)
+    {
+        if(H[j] < H[j+1])
+            j = j+1;
+
+        if(H[i] > H[j])
+        {
+            int t = H[i];
+            H[i] = H[j];
+            H[j] = t;
+
+            i = j;
+            j = j*2;
+        }
+        else
+            break;
+    }
+    return val;
+}
--- Data Structures/Sorting/Quick Sort.c
+++ Data Structures/Sorting/Quick Sort.c
@@ -47,38 +47,59 @@ void display_list(int *a)
         printf(" %d ", a[i]);
 }
 
-void quick_sort(int *a, int l, int h)
+void quick_sort(int *a, int l, int h)//Array to be sorted, lower & upper bounds of
+//array
 {
-    int j;
-    if (l < h)
+    //Idea here is take the first element as pivot, partition the list such that
+    //elements smaller than pivot are to its less and elements greater than pivot
+    //are to its right. We now have the pivot element at its appropriate place.
+    //Then repeat the same for elements to its left and to its left
+    
+    int j;//The pivot element
+
+    if (l < h)//There are at least 2 elements in the list
     {
-        j = partition(a, l, h);
-        quick_sort(a, l, j);
-        quick_sort(a, j + 1, h);
+        j = partition(a, l, h);//partition the list around pivot, pivot will
+        //be taken element at index 'l'. Function will return index of pivot, use
+        //to sort left and right sides
+
+        quick_sort(a, l, j);//Sorting left side
+
+        quick_sort(a, j + 1, h);//Sorting right side
     }
 }
 
 int partition(int *a, int l, int h)
 {
-    int pivot = a[l];
+    int pivot = a[l];//Element around which list will be partitioned
+
     int i = l, j = h;
+
+    //Idea here is, take 2 pointers(logical not programatical) at index of pivot or
+    //after pivot, 'i' here and one at end of list 'j' here. Increment 'i' until
+    //an element greater than pivot is found and decrement 'j' until an element
+    //greater than pivot is found. Swap elements at indexes 'i', 'j'.
+    //Repeat this until i>j, i.e. whole list is traversed.
+    //Now swap pivot and element at index 'j' and boom, we have piviot at its place.
+    //Return piviot's position :)
 
     do
     {
-        do{i++;}while(a[i] <= pivot);
-        do{j--;}while(a[j] > pivot);
+        do{i++;}while(a[i] <= pivot);//Increment 'i' until greater element is found
+        do{j--;}while(a[j] > pivot);//Decrement 'j' until smaller element is found
 
-        if(i < j)
-            swap(&a[i],&a[j]);
+        if(i < j)//Check if we haven't finished checking the whole list cuz when we do
+        //'i' will be greater than 'j'.
+            swap(&a[i],&a[j]);//Swap elements at indexes 'i' and 'j'
 
-    }while(i<j);
+    }while(i<j);//Repeat until whole list is visited
 
-    swap(&a[l],&a[j]);
+    swap(&a[l],&a[j]);//Swap element at 'j' and piviot for boom(piviot at its place)
 
-    return j;
+    return j;//return piviot's index.
 }
 
-void swap(int *a, int *b)
+void swap(int *a, int *b)//Swapping variables :)
 {
     int t = *a;
     *a=*b;
