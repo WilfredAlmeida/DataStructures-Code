ef25d396110a9f304d59c6bb81e8f12bdb5334d3
WilfredAlmeida <almeidawilfred642@gmail.com>
2020-08-20 15:03:16 +0530

    Modified: Iterative Merge Sort

--- Data Structures/Sorting/Merge Sort- Iterative.c
+++ Data Structures/Sorting/Merge Sort- Iterative.c
@@ -1,6 +1,6 @@
 /*
 
-Refer: Abdul Bari DS
+Refer: Abdul Bari DS for explaination, code from GFG
 
 */
 
@@ -13,7 +13,7 @@ void swap(int *a, int *b);
 
 int main()
 {
-    int arr[] = {8, 7, 6, 5, 4, 3, 2, 1};
+    int arr[] = {100, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
     size_t n = sizeof(arr) / sizeof(arr[0]);
 
     printf("\nUnsorted List:\n");
@@ -21,7 +21,7 @@ int main()
 
     merge_sort(arr, n);
 
-    printf("\nSorted List\n");
+    printf("\nSorted List:\n");
     print_array(arr, n);
 
     return 0;
@@ -35,21 +35,16 @@ void print_array(int arr[], int n)
 
 void merge_sort(int arr, int n)
 {
-    int l = 0, m = 0, h = 0, p = 0;
-
-    for (p = 2; p <= n; p = p * 2)
+    for (int i = 1; i <= n - 1; i *= 2)
     {
-        for (int i = 0; i + p - 1 <= n; i = i + p)
+        for (int j = 0; j < n - i; j += i * 2)
         {
-            l = i;
-            h = i + p - 1;
-            m = (l + h) / 2;
-
+            int l = j;
+            int m = min(j + i - 1, n - 1);
+            int h = min(l + i + i - 1, n - 1);
             merge(arr, l, m, h);
         }
     }
-    if (p / 2 < n)
-        merge(arr, 0, p / 2 - 1, n);
 }
 
 void merge(int arr[], int l, int m, int h)
@@ -85,5 +80,5 @@ void swap(int *a, int *b)
 
 int min(int a, int b)
 {
-    return (a>b)?a:b;
+    return (a < b) ? a : b;
 }
